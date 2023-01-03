a06fa1372912e777898d2f3d6e64333e6f27f724
Judy-Choi <mjchoi0831@gmail.com>
2023-01-04 00:31:41 +0900

    카테고리별 제품 리스트 구하는 API 중간저장

--- routes/productListRouter.js
+++ routes/productListRouter.js
@@ -6,6 +6,10 @@ const router = express.Router();
 // Best top6 개 제품 데이터를 제공하는 API
 router.get("/best", productListController.bestProduct);
 
+// 프론트로부터 카테고리 id를 request parameter 로 받아 제품 데이터를 제공하는 API
+// ex) 정육각 구현 : /list?tab=pork
+router.get("/list/:category_id", productListController.productList);
+
 module.exports = {
   router,
 };
