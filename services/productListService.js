56db6264075803fd6a2c4b34df8dcd5822d8efd2
Judy-Choi <mjchoi0831@gmail.com>
2023-01-04 00:25:14 +0900

    메인페이지 top6 판매제품 구하는 API 중간저장

--- services/productListService.js
+++ services/productListService.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const productListDao = require("../models/cartDao");
+const productListDao = require("../models/productListDao");
 
 // 메인페이지에 노출할 판매량 상위 제품을 DB에서 찾아 배열에 담아 리턴
 // 리턴 형태 : [{썸네일 : ~, 제품명 : ~, 가격 : ~, 기준무게/부피/갯수 : ~ }...]
