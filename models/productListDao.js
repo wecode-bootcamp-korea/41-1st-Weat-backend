a1cf9f365e2f555c6b9a49da82521ae33bd241e4
Judy-Choi <mjchoi0831@gmail.com>
2023-01-05 09:37:46 +0900

    sql 쿼리 구현

--- models/productListDao.js
+++ models/productListDao.js
@@ -3,17 +3,17 @@ const { myDataSource } = require("./myDataSource");
 // 메인페이지에 노출할 판매량 상위 제품을 DB에서 찾아 배열에 담아 리턴
 // 리턴 형태 : [{썸네일 : ~, 제품명 : ~, 가격 : ~, 기준무게/부피/갯수 : ~ }...]
 // 상위 6개 제품만 리턴
+// SQL ex : SELECT id FROM likes ORDER BY post_id DESC LIMIT 6;
 const bestProduct = async () => {
   try {
     return await myDataSource.query(
       `SELECT 
-      users.id AS thumbnail, 
-      users.profileImageUrl AS name,
-      posts.id AS price,
-      posts.imageUrl AS 기준무게/부피/갯수
+      thumbnail_image, 
+      name,
+      price,
+      base_unit
       FROM products
-      INNER JOIN posts ON posts.userId = users.id
-      ORDER BY 판매량
+      ORDER BY sold DESC
       LIMIT 6`
     );
   } catch (err) {
