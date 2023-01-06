6a410109f9f0a1dcd27396185e795e0b679e4ac4
Judy-Choi <mjchoi0831@gmail.com>
2023-01-06 15:28:47 +0900

    피드백 반영

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,20 +4,20 @@ const productController = require("../controllers/productController");
 const router = express.Router();
 
 // Best top6 개 제품 데이터를 제공하는 API
-// router.get("/best", productListController.productList);
+// router.get("/best", productController.productList);
 
 // 프론트로부터 카테고리 id를 request parameter 로 받아 제품 데이터를 제공하는 API
-// ex) 정육각 구현 : /list?tab=pork
+// ex) 정육각 구현 : /products?tab=pork
 
-// postman) localhost:3000/lists?category=pork&page=2&order=
+// postman) localhost:3000/products?category=pork&page=2&order=
 // 카테고리 기본화면
-// url/lists
+// url/products
 // 메인화면 top6
-// url/lists?best
+// url/products?best
 // 선택 카테고리 없음(기본값 돼지), 첫 페이지(기본값, 1)
-// url/lists
+// url/products
 // 선택 카테고리 = 1(pork), 2페이지
-// url/lists?category=1&page=2
+// url/products?category=1&page=2
 
 router.get("/", productController.productList);
 
