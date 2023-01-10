00ea3d1e8b63191cd8a70623ebb17f7e819924ea
Judy <53294075+Judy-Choi@users.noreply.github.com>
2023-01-10 16:33:25 +0900

    Update controllers/orderController.js
    Co-authored-by: Kang-kyunghun <68374847+Kang-kyunghun@users.noreply.github.com>

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,7 +2,7 @@ const orderService = require("../services/orderService");
 const { asyncErrorHandler } = require("../middlewares/errorHandling");
 
 // 주문내역 조회
-const read = asyncErrorHandler(async (req, res) => {
+const readOrderList = asyncErrorHandler(async (req, res) => {
   const orderList = await orderService.read(req.userId);
   return res.status(201).json(orderList);
 });
