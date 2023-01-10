8c956c76027cbd0c7a498e9bdc3f353a852bbfd6
Judy-Choi <mjchoi0831@gmail.com>
2023-01-10 17:50:51 +0900

    merge 후  수정

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,13 @@ const userRouter = require("./userRouter");
 // 제품 리스트 관련 라우터
 const productRouter = require("./productRouter");
 // 장바구니 관련 라우터
-const cartsRouter = require("./cartRouter");
+const cartRouter = require("./cartRouter");
 // 결제 관련 라우터
-const ordersRouter = require("./orderRouter");
+const orderRouter = require("./orderRouter");
 
-router.use("/users", usersRouter.router);
-router.use("/products", productsRouter.router);
-router.use("/carts", cartsRouter.router);
-router.use("/orders", ordersRouter.router);
+router.use("/users", userRouter.router);
+router.use("/products", productRouter.router);
+router.use("/carts", cartRouter.router);
+router.use("/orders", orderRouter.router);
 
 module.exports = router;
